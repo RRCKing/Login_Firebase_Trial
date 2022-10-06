7b0623ecca6ca8949257f47056ef5927be93ab89
KingChan <bassking123456@gmail.com>
2022-10-06 18:50:53 -0500

    Fine Tune Layout and Naming Convention

--- app/src/main/java/com/example/login_firebase_trial/MainActivity.kt
+++ app/src/main/java/com/example/login_firebase_trial/MainActivity.kt
@@ -9,22 +9,27 @@ import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+    // Declare the Firebase Authentication
     private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize the Firebase Authentication
         auth = FirebaseAuth.getInstance()
 
+        // Retrieve the account email and display name from the intent passed in
         val email = intent.getStringExtra("email")
         val displayName = intent.getStringExtra("name")
 
-        findViewById<TextView>(R.id.textView).text = email + "\n" + displayName
+        // Retrieve the Account Info TextView and set the account info there
+        findViewById<TextView>(R.id.textAccountInfo).text = displayName + "\n" + email
 
-        findViewById<Button>(R.id.signOutBtn).setOnClickListener{
+        // Retrieve the Sign Out button and set the on click listener to sign out
+        findViewById<Button>(R.id.btnSignOut).setOnClickListener{
             auth.signOut()
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, SignInActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/example/login_firebase_trial/SignUpActivity.kt
+++ app/src/main/java/com/example/login_firebase_trial/SignUpActivity.kt
@@ -10,33 +10,49 @@ import com.google.firebase.auth.FirebaseAuthException
 
 class SignUpActivity : AppCompatActivity() {
 
+    // Declare the data binding
     private lateinit var binding:ActivitySignUpBinding
+
+    // Declare the Firebase Authentication
     private lateinit var firebaseAuth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize the binding
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Initialize the Firebase Authentication
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.textView.setOnClickListener{
+        // Set the onClickListener for the going back to Sign In Activity TextView
+        binding.tvAlreadyRegister.setOnClickListener{
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
-        binding.button.setOnClickListener {
-            val email = binding.emailEt.text.toString()
-            val pass = binding.passET.text.toString()
-            val confirmPass = binding.confirmPassEt.text.toString()
 
+        // Set the on click event for the Sign Up button
+        binding.btnSignUp.setOnClickListener {
+
+            // Assign the email, password, and confirm password to the variables
+            val email = binding.etEmail.text.toString()
+            val pass = binding.etPassword.text.toString()
+            val confirmPass = binding.etConfirmPassword.text.toString()
+
+            // Check if the inputs are not empty, or else prompt a toast message
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                // Check if the password match the confirm password
                 if (pass == confirmPass) {
+                    // Create the user account
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener{
+                        // If successful, set the intent to go to the SignIn Activity
                         if (it.isSuccessful) {
                             val intent = Intent(this, SignInActivity::class.java)
                             startActivity(intent)
                         }else{
+                            // If not successful, prompt a toast message
                             Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
                         }
                     }
